b690a9c7f0152df85790f2d0d8adb0503bbf63a8
JustinA117 <94567481+JustinA117@users.noreply.github.com>
2022-06-02 13:02:32 -0700

    Add files via upload

--- PixelPlayground.java
+++ PixelPlayground.java
@@ -1,201 +1,127 @@
 import java.awt.Color;
 
+
+
 public class PixelPlayground {
+
+public static Picture zeroBlue (Picture p){
+Picture newPic = new Picture(p);
+
+// STEP 1 - get 2D array of pixels using existing getPixels2D() method. 
+Pixel [][] pixels = newPic.getPixels2D();
+
+//STEP 2- traverse through all pixels(all rows and columns) using for loops 
+for (int r = 0; r <pixels.length; r++)
+{
+for (int c = 0; c<pixels[0].length; c++)
+{pixels[r][c].setBlue(0); 
+/* for (Pixel[] row: pixels)
+{
+for (Pixel column: row )
+} */
+}
+
+// STEP 3 - set blue to 0 using existing setBlue() method
+
+    }
+  return newPic; 
+  
+}
  
-    public static Picture zeroBlue(Picture p) {
-        
-        Picture newPic = new Picture(p);
 
-        Pixel [][] pixels = newPic.getPixels2D();
+public static Picture keepOnlyBlue (Picture a){
+    Picture myPic = new Picture(a); 
+    Pixel [][] pixels = myPic.getPixels2D(); 
+    for (int r = 0; r <pixels.length; r++)
+    {
+    for (int c = 0; c<pixels[0].length; c++)
+    {
+        pixels[r][c].setRed(0); 
+        pixels[r][c].setGreen(0); 
+}
+    }
+   return myPic;
+}
 
-        for(int r = 0; r < pixels.length; r++)
-        {
-            //finds the row length. V
-                for(int c = 0; c<pixels[0].length; c++)
-                {
-                    pixels[r][c].setBlue(0);
-
-                }
+public static Picture negatePicture(Picture a){
 
 
+Picture ourPic = new Picture(a); 
+Pixel [][] pixels = ourPic.getPixels2D(); 
+for (int r = 0; r <pixels.length; r++)
+{
+for (int c = 0; c<pixels[0].length; c++)
+{
+   int numRed = pixels[r][c].getRed();
+   int numGreen = pixels[r][c].getGreen(); 
+   int numBlue = pixels [r][c].getBlue(); 
+    pixels[r][c].setRed(255- numRed);
+    pixels[r][c].setGreen(255- numGreen);
+    pixels[r][c].setGreen(255- numBlue);
+}   
+    }
+return ourPic;
 
-        }
-        
-        return newPic;
+}
+public static Picture grayScale(Picture a){
+    Picture theirPic = new Picture(a); 
+    Pixel [][] pixels = theirPic.getPixels2D(); 
+    for (int r = 0; r <pixels.length; r++)
+    {
+    for (int c = 0; c<pixels[0].length; c++)
+    {
+        pixels[r][c].setRed((pixels[r][c].getRed() +  pixels[r][c].getGreen() + pixels [r][c].getBlue())/3);
+        pixels[r][c].setGreen((pixels[r][c].getRed() +  pixels[r][c].getGreen() + pixels [r][c].getBlue())/3); 
+        pixels[r][c].setBlue((pixels[r][c].getRed() +  pixels[r][c].getGreen() + pixels [r][c].getBlue())/3); 
+    }
+}   return theirPic;
+
+}
+
+public static Picture mirrorImageHoiz1(Picture p){
+    /** Mirrors left part of image to the right side 
+     * 
+     * 
+     */
+    Picture newPic = new Picture (p); // copy p as not to destroy persistent data 
+    Pixel [][] pixels = newPic.getPixels2D(); 
+    for (int r = 0; r <pixels.length; r++)
+    {
+    for (int c = 0; c<pixels[0].length/2; c++)
+    {
+        pixels[r][pixels[0].length-1-c].setColor(pixels[r][c].getColor());
 
     }
+}
+return newPic; 
 
-    public static Picture keepOnlyBlue (Picture a){
-        Picture myPic = new Picture(a);
-        Pixel [][] pixels = myPic.getPixels2D();
-        for(int r = 0; r < pixels.length; r++)
-        {
-            //finds the row length. V
-                for(int c = 0; c<pixels[0].length; c++)
-                {
-                    pixels[r][c].setRed(0);
-                    pixels[r][c].setGreen(0);
-
-                }
+}
 
 
+public static void main(String[] args)
 
-        }
-        return myPic;
-    }
-    public static Picture negatePicture (Picture a){
-        Picture ourPic = new Picture(a);
-        Pixel [][] pixels = ourPic.getPixels2D();
-        for(int r = 0; r < pixels.length; r++)
-        {
-            //finds the row length. V
-                for(int c = 0; c<pixels[0].length; c++)
-                {
-                    int numRed=pixels[r][c].getRed();
-                    int numGreen=pixels[r][c].getGreen();
-                    int numBlue=pixels[r][c].getBlue();
-                    pixels[r][c].setRed(255-numRed);
-                    pixels[r][c].setGreen(255-numGreen);
-                    pixels[r][c].setBlue(255-numBlue);
-                }
-
-
-
-        }
-        return ourPic;
-    }
-    public static Picture greyscalePicture (Picture a){
-        Picture hmmPic = new Picture(a);
-        Pixel [][] pixels = hmmPic.getPixels2D();
-        for(int r = 0; r < pixels.length; r++)
-        {
-            //finds the row length. V
-                for(int c = 0; c<pixels[0].length; c++)
-                {
-                    int numRed=pixels[r][c].getRed();
-                    int numGreen=pixels[r][c].getGreen();
-                    int numBlue=pixels[r][c].getBlue();
-                    int avgColor= (numBlue + numGreen + numRed)/3;
-                    pixels[r][c].setRed(avgColor);
-                    pixels[r][c].setGreen(avgColor);
-                    pixels[r][c].setBlue(avgColor);
-                }
-
-
-
-        }
-        return hmmPic;
-    }
-    public static Picture horizonalMirroredImage (Picture a){
-        Picture yourPic = new Picture(a);
-        Pixel [][] pixels = yourPic.getPixels2D();
-        for(int r = 0; r < pixels.length; r++)
-        {
-            //finds the row length. V
-                for(int c = 0; c<pixels[0].length; c++)
-                {
-                    
-                   pixels[r][pixels[0].length-1-c].setColor(pixels[r][c].getColor());
-                }
-//take the length then find the index of point a and then subtract the index of from the length to gets its mirrored.
-
-
-        }
-        return yourPic;
-    }
-    public static Picture verticalMirroredImage (Picture a){
-        Picture theirPic = new Picture(a);
-        Pixel [][] pixels = theirPic.getPixels2D();
-        for(int r = 0; r < pixels.length/2; r++)
-        {
-            //finds the row length. V
-                for(int c = 0; c<pixels[0].length; c++)
-                {
-                    
-                   pixels[pixels.length-1-r][c].setColor(pixels[r][c].getColor());
-                }
-//take the length then find the index of point a and then subtract the index of from the length to gets its mirrored.
-
-
-        }
-        return theirPic;
-    }
-    public static Picture verticalMirroredImage2 (Picture a){
-        Picture okPic = new Picture(a);
-        Pixel [][] pixels = okPic.getPixels2D();
-        for(int r = 0; r < pixels.length/2; r++)
-        {
-            //finds the row length. V
-            
-                for(int c = 0; c<pixels[0].length; c++)
-                {
-                    
-                   pixels[pixels.length-1-r][c].setColor(pixels[r][c].getColor());
-                }
-//take the length then find the index of point a and then subtract the index of from the length to gets its mirrored.
-
-
-        }
-        return okPic;
-    }
-
-
-
-
-public static void main(String[] args){
-
- 
+ {
 
 Picture beachPic = new Picture("beach.jpg");
 
-Picture hiddenMsg = new Picture("blueMotorcycle.jpg");
-hiddenMsg.explore();
 beachPic.explore();
 
-//Picture swanPic = new Picture("swan.jpg");
-//swanPic.explore();
+Picture beachPicNoBlue = zeroBlue(beachPic);
 
-//Picture Barb = new Picture("barbaraS.jpg");
-//Barb.explore();
+beachPicNoBlue.explore();
 
+Picture beachPicOnlyBlue = keepOnlyBlue(beachPic);
+beachPicOnlyBlue.explore();
 
-//Picture beachPicNoBlue = zeroBlue(beachPic);
+Picture beachPicNegate = negatePicture(beachPic);
+beachPicNegate.explore();
 
-//beachPicNoBlue.explore();
+Picture beachPicGrayScale = negatePicture(beachPic);
+beachPicGrayScale.explore();
 
-//Picture beachOnlyBlue = keepOnlyBlue(beachPic);
-
-//beachOnlyBlue.explore();
-
-//Picture beachNegated = negatePicture(beachPic);
-
-//beachNegated.explore();
-
-//Picture beachMirroredImageHorizonal = horizonalMirroredImage(beachPic);
-//beachMirroredImageHorizonal.explore();
-
-//Picture swanMirrored = horizonalMirroredImage(swanPic);
-//swanMirrored.explore();
-
-//Picture swanMirroredVert = verticalMirroredImage(swanPic);
-//swanMirroredVert.explore();
-
-//Picture greyBeach = greyscalePicture(beachPic);
-//greyBeach.explore();
-
-//Picture BarbruhS = Stegonography.testSetLow(beachPic, Color.pink);
-//BarbruhS.explore();
-
-//Picture copy3 = Stegonography.revealPicture(BarbruhS);
-//copy3.explore(); 
-
-Picture hideThePicture = Stegonography.hidePicture(beachPic, hiddenMsg);
-hideThePicture.explore();
-
-Picture revealedPic = Stegonography.revealPicture(hideThePicture);
-revealedPic.explore(); 
-
+Picture beachPicMirror = mirrorImageHoiz1(beachPic);
+beachPicMirror.explore(); 
+ }
 
 }
-}
- 
+
